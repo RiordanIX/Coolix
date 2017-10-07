a7c44d690103ffaa3ea7e54c445d009a782e9a4d
Major <majorjctaylor@gmail.com>
2017-10-07 00:46:00 -0400

    Added some decoding ability
    Advanced our ability to decode the instruction.  This uses bitmasking to find the opcode and instruction type.  After the instruction type is discovered, the instruction is passed to the instruction type operation method to find the exact opcode and then to perform the operation.

--- src/cpu.cpp
+++ src/cpu.cpp
@@ -1,8 +1,24 @@
 #include "cpu.hpp"
 
-void cpu::execute(instruct_t inst) {
-	// TODO Actually execute something
+void cpu::decode(instruct_t inst) {
 	std::cout << inst << std::endl;
+	instruct_t format_code = inst & FORMAT_CODE_MASK;
+	if (format_code == INST_ARITHMETIC) {
+		cpu_arithmetic_operation(inst);
+	}
+	else if (format_code == INST_CONDTL_AND_IMMEDIATE) {
+		cpu_condtl_and_immediate_operation(inst);
+	}
+	else if (format_code == INST_UNCONDITIONAL) {
+		cpu_unconditional_operation(inst);
+	}
+	else if (format_code == INST_IO) {
+		cpu_io_operation(inst);
+	}
+	else {
+		std::cout << get_info() << std::endl;
+		throw "Error while decoding instruction";
+	}
 }
 
 
@@ -13,4 +29,5 @@ std::string cpu::get_info() {
 	}
 	return info;
 }
+
 
