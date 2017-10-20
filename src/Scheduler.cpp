23a63c93fc26d1faa349fc280dff6371659be524
Major <majorjctaylor@gmail.com>
2017-10-20 13:59:29 -0400

    Moved page_table to upper dir. Removed extra main

--- src/Scheduler.cpp
+++ src/Scheduler.cpp
@@ -1,13 +1,14 @@
 // Scheduler.cpp : Defines the entry point for the console application.
 //
-
-#include "stdafx.h"
-#include "PCB.h"
-#include "Priority.h"
-#include "FIFO.h"
-
-int main()
-{
-	return 0;
-}
-
+// This should not be the an entry point.  The entry point is the init.cpp
+//
+//#include "stdafx.h"
+//#include "PCB.h"
+//#include "Priority.h"
+//#include "FIFO.h"
+//
+//int main()
+//{
+//	return 0;
+//}
+//
