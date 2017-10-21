49b1767ffed80932aee8740c84ce88ff7b1c6727
Umarrkhan786 <Ltsystems.control@gmail.com>
2017-10-21 19:25:50 -0400

    Add files via upload

--- src/dispatcher.cpp
+++ src/dispatcher.cpp
@@ -3,8 +3,7 @@
 
 void Dispatcher::dispatch(cpu* CPU, PCB* cProcess, int cCycle, int maxCycle) {
 	// If the readyQueue is not empty
-	if(!readyQueue.empty()
-		&& ((cProcess->get_status() == status::WAITING) || (cCycle >= maxCycle)))
+	if (!readyQueue.empty() &&( (cProcess->get_status() == status::WAITING) || (cCycle >= maxCycle)))
 	{
 		switchOut(CPU, cProcess);   //  Moves current process to WaitingQueue
 		switchIn(CPU);			  //  Removes First Process and gives next process to CPU
@@ -23,10 +22,11 @@ void Dispatcher::switchIn(cpu* CPU) {
 	// Removes the Current Running Process, so the next one will be the new active process
 	readyQueue.removeProcess();
 	// Sets the CPU registers to the new PCB registers
-	CPU->set_registers( readyQueue.getProcess()->get_registers());
+	if (!readyQueue.empty())
+	{
+		CPU->set_registers(readyQueue.getProcess()->get_registers());
+	}
 	//  Assigns the new First Process in the Ready Queue to the current Process
 	//  in the CPU
 	//CPU.CurrentProcess = readyQueue.getProcess();
 }
-
-
