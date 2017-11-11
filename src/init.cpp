10e786f772c308ad9265f995cfeabbc9bf6ec1b7
bernandosoares <blinxwang@gmail.com>
2017-11-10 19:50:11 -0500

    allow jobfile to be specified when compiled with debug flags

--- src/init.cpp
+++ src/init.cpp
@@ -4,8 +4,8 @@
 int main(int argc, char* argv[]) {
 #if (defined _WIN32 || defined DEBUG)
 	std::cout << argc << '\n' << argv << std::endl;
-	OSDriver driver;
-	driver.run("test_job2");
+	// OSDriver driver;
+	// driver.run("test_job2");
 #else
 	if (argc == 1) {
 		std::cout << "Usage:  coolix <run_file>" << std::endl;
