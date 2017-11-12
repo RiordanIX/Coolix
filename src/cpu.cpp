a03e977305dcc55d5fddf7dba6f645c729bcaa1a
Umarrkhan786 <Ltsystems.control@gmail.com>
2017-11-12 16:58:30 -0500

    Add files via upload
    All 30 jobs work

--- src/cpu.cpp
+++ src/cpu.cpp
@@ -8,11 +8,11 @@
 extern Ram MEM;
 
 void cpu::decode_and_execute(instruct_t inst, PCB* pcb) {
-	debug_printf("This Instruction: %#010X\n",inst);
+	debug_printf("This Instruction: %#010X\n", inst);
 
 	instruct_t format_code = inst & FORMAT_CODE_MASK;
 	instruct_t opcode = inst & OPCODE_MASK;
-	opcode = opcode >> (3*8); // 3 bytes to get to lowest position
+	opcode = opcode >> (3 * 8); // 3 bytes to get to lowest position
 
 	if (format_code == INST_ARITHMETIC) {
 		cpu_arithmetic_operation(inst, opcode);
@@ -47,7 +47,7 @@ std::string cpu::get_info() {
 
 void cpu::set_registers(std::vector<instruct_t> source) {
 	int i = 0;
-	for (auto it = source.begin(); it != source.end(); it++){
+	for (auto it = source.begin(); it != source.end(); it++) {
 		registers[i] = *it;
 		i++;
 	}
@@ -57,63 +57,63 @@ void cpu::set_registers(std::vector<instruct_t> source) {
 inline void cpu::cpu_rd(instruct_t Reg1, instruct_t Reg2, instruct_t Address, instruct_t offset) {
 	//registers[Reg1] = (Address == 0) ? registers[Reg2] : MEM.get_instruction(Address + offset);
 	if (Reg2 > 0) {
-		registers[Reg1] = MEM.get_instruction(registers[Reg2]+offset);
+		registers[Reg1] = MEM.get_instruction(registers[Reg2] + offset);
 	}
 	else {
 		registers[Reg1] = MEM.get_instruction(Address + offset);
-		}
+	}
 }
 
 
 inline void cpu::cpu_wr(instruct_t Reg1, instruct_t Reg2, instruct_t Address, instruct_t offset) {
-	if(Reg2 > 0)
+	if (Reg2 > 0)
 		registers[Reg2] = registers[Reg1];
 	else {
 		debug_printf("Writing: %#010X (%d in decimal), write location: %#010X\n",
-				registers[Reg1], registers[Reg1], Address);
+			registers[Reg1], registers[Reg1], Address);
 		debug_printf("End address: %#010X\n", offset);
 		MEM.allocate(Address + offset, registers[Reg1]);
 	}
 }
 
 
-inline void cpu::cpu_mov(instruct_t s1, instruct_t dest ) {
+inline void cpu::cpu_mov(instruct_t s1, instruct_t dest) {
 	registers[dest] = registers[s1];
 }
 
-inline void cpu::cpu_add(instruct_t s1, instruct_t s2, instruct_t dest ) {
+inline void cpu::cpu_add(instruct_t s1, instruct_t s2, instruct_t dest) {
 	registers[dest] = registers[s1] + registers[s2];
 }
 
-inline void cpu::cpu_sub(instruct_t s1, instruct_t s2, instruct_t dest ) {
+inline void cpu::cpu_sub(instruct_t s1, instruct_t s2, instruct_t dest) {
 	registers[dest] = registers[s1] - registers[s2];
 }
 
-inline void cpu::cpu_mul(instruct_t s1, instruct_t s2, instruct_t dest ) {
+inline void cpu::cpu_mul(instruct_t s1, instruct_t s2, instruct_t dest) {
 	registers[dest] = registers[s1] * registers[s2];
 }
 
-inline void cpu::cpu_div(instruct_t s1, instruct_t s2, instruct_t dest ) {
+inline void cpu::cpu_div(instruct_t s1, instruct_t s2, instruct_t dest) {
 	registers[dest] = registers[s1] / registers[s2];
 }
 
 // Logical AND
-inline void cpu::cpu_and(instruct_t s1, instruct_t s2, instruct_t dest ) {
+inline void cpu::cpu_and(instruct_t s1, instruct_t s2, instruct_t dest) {
 	registers[dest] = registers[s1] & registers[s2];
 }
 
 // Logical OR
-inline void cpu::cpu_or(instruct_t s1, instruct_t s2, instruct_t dest ) {
+inline void cpu::cpu_or(instruct_t s1, instruct_t s2, instruct_t dest) {
 	registers[dest] = registers[s1] | registers[s2];
 }
 
 // Set value to 1 if s1 < s2, 0 otherwise
-inline void cpu::cpu_slt(instruct_t s1, instruct_t s2, instruct_t dest ) {
+inline void cpu::cpu_slt(instruct_t s1, instruct_t s2, instruct_t dest) {
 	registers[dest] = registers[s1] < registers[s2] ? 1 : 0;
 }
 
-inline void cpu::cpu_st(instruct_t B_reg, instruct_t D_reg) {
-	MEM.allocate(registers[D_reg], registers[B_reg]);
+inline void cpu::cpu_st(instruct_t B_reg, instruct_t D_reg, size_t offset) {
+	MEM.allocate(offset + registers[D_reg], offset + registers[B_reg]);
 }
 
 inline void	cpu::cpu_lw(instruct_t B_reg, instruct_t D_reg, instruct_t Address) {
@@ -183,4 +183,3 @@ inline void	cpu::cpu_blz(instruct_t B_reg, instruct_t Address, PCB* pcb) {
 	if (registers[B_reg] & 0x80000000)
 		pcb->set_program_counter(Address - 4);
 }
-
--- src/cpu.hpp
+++ src/cpu.hpp
@@ -13,18 +13,18 @@
 
 
 /******************************************************************************
- * cpu
- * A simulated CPU class. Simulates some aspects of a normal CPU.  The user
- * can define the number of registers by passing it in, or just leaving it as
- * the default of 16.
- *****************************************************************************/
+* cpu
+* A simulated CPU class. Simulates some aspects of a normal CPU.  The user
+* can define the number of registers by passing it in, or just leaving it as
+* the default of 16.
+*****************************************************************************/
 class cpu {
 public:
 	// Public because the dispatcher needs to access it.
 	std::size_t num_registers;
 	std::vector<instruct_t> registers;
 
-	cpu(std::size_t size=DEFAULT_REG_SIZE) : num_registers(size), registers(size, 0) {}
+	cpu(std::size_t size = DEFAULT_REG_SIZE) : num_registers(size), registers(size, 0) {}
 	instruct_t fetch(PCB* pcb);
 	void set_registers(std::vector<instruct_t> to_switch);
 	void decode_and_execute(instruct_t inst, PCB* pcb);
@@ -32,42 +32,42 @@ public:
 
 
 	/**************************************************************************
-	 * Emulated cpu operations
-	 * The following functions are used to emulate the operations of the cpu
-	 * itself.  They should all defined as inline, and they should only have a
-	 * couple lines at most.
-	 *************************************************************************/
+	* Emulated cpu operations
+	* The following functions are used to emulate the operations of the cpu
+	* itself.  They should all defined as inline, and they should only have a
+	* couple lines at most.
+	*************************************************************************/
 	/**************************************************************************
-	 *  I/O instructions {{{
-	 *************************************************************************/
+	*  I/O instructions {{{
+	*************************************************************************/
 	void cpu_rd(instruct_t Reg1, instruct_t Reg2, instruct_t Address, instruct_t offset);
 	void cpu_wr(instruct_t Reg1, instruct_t Reg2, instruct_t Address, instruct_t offset);
 	/**************************************************************************
-	 * }}} End I/O instructions
-	 *************************************************************************/
+	* }}} End I/O instructions
+	*************************************************************************/
 
 
 	/**************************************************************************
-	 *  Arithmetic instructions {{{
-	 *************************************************************************/
-	void cpu_mov(instruct_t s1, instruct_t dest );
-	void cpu_add(instruct_t s1, instruct_t s2, instruct_t dest );
-	void cpu_sub(instruct_t s1, instruct_t s2, instruct_t dest );
-	void cpu_mul(instruct_t s1, instruct_t s2, instruct_t dest );
-	void cpu_div(instruct_t s1, instruct_t s2, instruct_t dest );
-	void cpu_and(instruct_t s1, instruct_t s2, instruct_t dest );
-	void cpu_or(instruct_t s1, instruct_t s2, instruct_t dest );
-	void cpu_slt(instruct_t s1, instruct_t s2, instruct_t dest );
+	*  Arithmetic instructions {{{
+	*************************************************************************/
+	void cpu_mov(instruct_t s1, instruct_t dest);
+	void cpu_add(instruct_t s1, instruct_t s2, instruct_t dest);
+	void cpu_sub(instruct_t s1, instruct_t s2, instruct_t dest);
+	void cpu_mul(instruct_t s1, instruct_t s2, instruct_t dest);
+	void cpu_div(instruct_t s1, instruct_t s2, instruct_t dest);
+	void cpu_and(instruct_t s1, instruct_t s2, instruct_t dest);
+	void cpu_or(instruct_t s1, instruct_t s2, instruct_t dest);
+	void cpu_slt(instruct_t s1, instruct_t s2, instruct_t dest);
 	/**************************************************************************
-	 * }}} End Arithmetic instructions
-	 *************************************************************************/
+	* }}} End Arithmetic instructions
+	*************************************************************************/
 
 
 	/**************************************************************************
-	 * Immediate instructions {{{
-	 * When the last 16 bits contain data, the D-reg is always 0000
-	 *************************************************************************/
-	void cpu_st(instruct_t B_reg, instruct_t D_reg);
+	* Immediate instructions {{{
+	* When the last 16 bits contain data, the D-reg is always 0000
+	*************************************************************************/
+	void cpu_st(instruct_t B_reg, instruct_t D_reg, size_t offset);
 	void cpu_lw(instruct_t B_reg, instruct_t D_reg, instruct_t Address);
 	void cpu_movi(instruct_t D_reg, instruct_t Address);
 	void cpu_addi(instruct_t B_reg, instruct_t D_reg, instruct_t Address);
@@ -84,169 +84,168 @@ public:
 	void cpu_bgz(instruct_t B_reg, instruct_t Address, PCB* pcb);
 	void cpu_blz(instruct_t B_reg, instruct_t Address, PCB* pcb);
 	/**************************************************************************
-	 * }}} End of Immediate instructions
-	 *************************************************************************/
+	* }}} End of Immediate instructions
+	*************************************************************************/
 
 
 	/**************************************************************************
-	 * Arithmetic Operations {{{
-	 * Determines which operation to use.
-	 * Essentially a glorified switch statement.  Separated out so easier to
-	 * read. Arithmetic operations all have the _R_ part in the opcode
-	 * definition
-	 *************************************************************************/
+	* Arithmetic Operations {{{
+	* Determines which operation to use.
+	* Essentially a glorified switch statement.  Separated out so easier to
+	* read. Arithmetic operations all have the _R_ part in the opcode
+	* definition
+	*************************************************************************/
 	inline void cpu_arithmetic_operation(instruct_t inst, instruct_t opcode) {
 		instruct_t S1, S2, Dest;
 		// Each hex position is worth 4 bits, so 5 Hexes = 5*4 bits
-		S1   = (inst & 0x00F00000) >> (5*4);
-		S2   = (inst & 0x000F0000) >> (4*4);
-		Dest = (inst & 0x0000F000) >> (3*4);
-		switch (opcode){
-			case OP_R_MOV:
-				cpu_mov(S2, Dest);
-				break;
-			case OP_R_ADD:
-				cpu_add(S1, S2, Dest);
-				break;
-			case OP_R_SUB:
-				cpu_sub(S1, S2, Dest);
-				break;
-			case OP_R_MUL:
-				cpu_mul(S1, S2, Dest);
-				break;
-			case OP_R_DIV:
-				cpu_div(S1, S2, Dest);
-				break;
-			case OP_R_AND:
-				cpu_and(S1, S2, Dest);
-				break;
-			case OP_R_OR:
-				cpu_or(S1, S2, Dest);
-				break;
-			case OP_R_SLT:
-				cpu_slt(S1, S2, Dest);
-				break;
-			default:
-				throw "Illegal arithmetic operation: " + std::to_string(inst);
+		S1 = (inst & 0x00F00000) >> (5 * 4);
+		S2 = (inst & 0x000F0000) >> (4 * 4);
+		Dest = (inst & 0x0000F000) >> (3 * 4);
+		switch (opcode) {
+		case OP_R_MOV:
+			cpu_mov(S2, Dest);
+			break;
+		case OP_R_ADD:
+			cpu_add(S1, S2, Dest);
+			break;
+		case OP_R_SUB:
+			cpu_sub(S1, S2, Dest);
+			break;
+		case OP_R_MUL:
+			cpu_mul(S1, S2, Dest);
+			break;
+		case OP_R_DIV:
+			cpu_div(S1, S2, Dest);
+			break;
+		case OP_R_AND:
+			cpu_and(S1, S2, Dest);
+			break;
+		case OP_R_OR:
+			cpu_or(S1, S2, Dest);
+			break;
+		case OP_R_SLT:
+			cpu_slt(S1, S2, Dest);
+			break;
+		default:
+			throw "Illegal arithmetic operation: " + std::to_string(inst);
 		}
 	}
 	/**************************************************************************
-	 * }}} End of Arithmetic Operations
-	 *************************************************************************/
+	* }}} End of Arithmetic Operations
+	*************************************************************************/
 
 
 	/**************************************************************************
-	 * Immediate Operations {{{
-	 *************************************************************************/
+	* Immediate Operations {{{
+	*************************************************************************/
 	inline void cpu_immediate_operation(instruct_t inst, instruct_t opcode, PCB* pcb) {
 		instruct_t B_reg, D_reg, Address;
-		B_reg   = (inst & 0x00F00000) >> (5*4);
-		D_reg   = (inst & 0x000F0000) >> (4*4);
-		Address =  inst & 0x0000FFFF;
+		B_reg = (inst & 0x00F00000) >> (5 * 4);
+		D_reg = (inst & 0x000F0000) >> (4 * 4);
+		Address = inst & 0x0000FFFF;
 
 		switch (opcode) {
-			case OP_I_ST:
-				cpu_st(B_reg, D_reg);
-				break;
-			case OP_I_LW:
-				cpu_lw(B_reg, D_reg, Address);
-				break;
-			case OP_I_MOVI:
-				cpu_movi( D_reg, Address);
-				break;
-			case OP_I_ADDI:
-				cpu_addi(B_reg, D_reg, Address);
-				break;
-			case OP_I_MULI:
-				cpu_muli(B_reg, D_reg, Address);
-				break;
-			case OP_I_DIVI:
-				cpu_divi( D_reg, Address);
-				break;
-			case OP_I_LDI:
-				cpu_ldi( D_reg, Address);
-				break;
-			case OP_I_SLTI:
-				cpu_slti(B_reg, D_reg, Address);
-				break;
-			case OP_I_BEQ:
-				cpu_beq(B_reg, D_reg, Address, pcb);
-				break;
-			case OP_I_BNE:
-				cpu_bne(B_reg, D_reg, Address, pcb);
-				break;
-			case OP_I_BEZ:
-				cpu_bez(B_reg, Address, pcb);
-				break;
-			case OP_I_BNZ:
-				cpu_bnz(B_reg, Address, pcb);
-				break;
-			case OP_I_BGZ:
-				cpu_bgz(B_reg, Address, pcb);
-				break;
-			case OP_I_BLZ:
-				cpu_blz(B_reg, Address, pcb);
-				break;
-			default:
-				throw "Invalid Immediate instruction format";
+		case OP_I_ST:
+			cpu_st(B_reg, D_reg,pcb->get_ram_address());
+			break;
+		case OP_I_LW:
+			cpu_lw(B_reg, D_reg, Address);
+			break;
+		case OP_I_MOVI:
+			cpu_movi(D_reg, Address);
+			break;
+		case OP_I_ADDI:
+			cpu_addi(B_reg, D_reg, Address);
+			break;
+		case OP_I_MULI:
+			cpu_muli(B_reg, D_reg, Address);
+			break;
+		case OP_I_DIVI:
+			cpu_divi(D_reg, Address);
+			break;
+		case OP_I_LDI:
+			cpu_ldi(D_reg, Address);
+			break;
+		case OP_I_SLTI:
+			cpu_slti(B_reg, D_reg, Address);
+			break;
+		case OP_I_BEQ:
+			cpu_beq(B_reg, D_reg, Address, pcb);
+			break;
+		case OP_I_BNE:
+			cpu_bne(B_reg, D_reg, Address, pcb);
+			break;
+		case OP_I_BEZ:
+			cpu_bez(B_reg, Address, pcb);
+			break;
+		case OP_I_BNZ:
+			cpu_bnz(B_reg, Address, pcb);
+			break;
+		case OP_I_BGZ:
+			cpu_bgz(B_reg, Address, pcb);
+			break;
+		case OP_I_BLZ:
+			cpu_blz(B_reg, Address, pcb);
+			break;
+		default:
+			throw "Invalid Immediate instruction format";
 		}
 	}
 	/**************************************************************************
-	 * }}} End of Immediate Operations
-	 *************************************************************************/
+	* }}} End of Immediate Operations
+	*************************************************************************/
 
 
 	/**************************************************************************
-	 * Unconditional Operations {{{
-	 *************************************************************************/
+	* Unconditional Operations {{{
+	*************************************************************************/
 	inline void cpu_unconditional_operation(instruct_t inst, instruct_t opcode, PCB* pcb) {
 		instruct_t Address;
-		Address =  inst & 0x00FFFFFF;
+		Address = inst & 0x00FFFFFF;
 		//offset  = pcb->get_ram_address();
 
-		switch(opcode)
+		switch (opcode)
 		{
-			case OP_J_JMP:
-				cpu_jmp(Address,/* offset,*/ pcb);
-				break;
+		case OP_J_JMP:
+			cpu_jmp(Address,/* offset,*/ pcb);
+			break;
 
-			case OP_J_HLT:
-				cpu_hlt(pcb);
-				break;
-			default:
-				throw "Invalid Unconditional Operation Format";
+		case OP_J_HLT:
+			cpu_hlt(pcb);
+			break;
+		default:
+			throw "Invalid Unconditional Operation Format";
 		}
 	}
 	/**************************************************************************
-	 * }}}  End of Unconditional Operations
-	 *************************************************************************/
+	* }}}  End of Unconditional Operations
+	*************************************************************************/
 
 
 	/**************************************************************************
-	 * IO Operations{{{
-	 *************************************************************************/
+	* IO Operations{{{
+	*************************************************************************/
 	inline void cpu_io_operation(instruct_t inst, instruct_t opcode, PCB* pcb) {
 		instruct_t Reg1, Reg2, Address, offset;
-		Reg1 =   (inst & 0x00F00000) >> (5*4);
-		Reg2 =   (inst & 0x000F0000) >> (4*4);
+		Reg1 = (inst & 0x00F00000) >> (5 * 4);
+		Reg2 = (inst & 0x000F0000) >> (4 * 4);
 		Address = inst & 0x0000FFFF;
 		offset = pcb->get_ram_address();
 
-		switch(opcode)
+		switch (opcode)
 		{
-			case OP_IO_RD:
-				cpu_rd(Reg1, Reg2, Address, offset);
-				break;
+		case OP_IO_RD:
+			cpu_rd(Reg1, Reg2, Address, offset);
+			break;
 
-			case OP_IO_WR:
-				cpu_wr(Reg1, Reg2, Address, offset);
-				break;
-			default:
-				throw "Invalid IO instruction format";
+		case OP_IO_WR:
+			cpu_wr(Reg1, Reg2, Address, offset);
+			break;
+		default:
+			throw "Invalid IO instruction format";
 		}
 	}
 	/**************************************************************************
-	 * }}} End of IO operations
-	 *************************************************************************/
+	* }}} End of IO operations
+	*************************************************************************/
 };
-
