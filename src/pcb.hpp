3ec94772f8f9eeb6cfe650420b4bbbae372f8e3b
Umer Abbasi <Umarrkhan786@hotmail.com>
2017-11-17 18:12:35 -0500

    fix the pop_lru method

--- src/pcb.hpp
+++ src/pcb.hpp
@@ -42,7 +42,7 @@ public:
 		std::vector<std::pair<bool, size_t>> pages;
 		PageTable(std::size_t size) : pages(size)
 		{
-			for(auto it = pages.begin(); it != pages.end(); it++)
+			for (auto it = pages.begin(); it != pages.end(); it++)
 			{
 				(*it).first = false;
 				(*it).second = -1;
@@ -60,57 +60,57 @@ public:	PCB(int id, std::size_t daddress, std::size_t instruct, std::size_t inp,
 	programCounter(0),
 	registers(16, 0),
 	sectionSizes(4, 0)
-    {
-		sectionSizes[section::INSTRUCTION] = instruct;
-		sectionSizes[section::INPUT] = inp;
-		sectionSizes[section::OUTPUT] = out;
-		sectionSizes[section::TEMP] = temp;
-		
-		//Give the page table enough pages to fit the process
-		pageTable = PCB::PageTable((get_end_address() / PAGE_SIZE + 1) * PAGE_SIZE);
-	}
-	// GETTERS
-	int get_priority() { return priority; }
-	unsigned int get_pid() { return pid; }
-	std::size_t get_ram_address() { return ramAddress; }
-	std::size_t get_disk_address() { return diskAddress; }
-	std::size_t get_inp_address() { return sectionSizes[INSTRUCTION]; }
-	std::size_t get_out_address() { return sectionSizes[INSTRUCTION] + sectionSizes[INPUT]; }
-	std::size_t get_temp_address() { return sectionSizes[INSTRUCTION] + sectionSizes[INPUT] + sectionSizes[OUTPUT]; }
-	std::size_t get_end_address() { return sectionSizes[INSTRUCTION] + sectionSizes[INPUT] + sectionSizes[OUTPUT] + sectionSizes[TEMP]; }
-	int get_wait_time() { return wait_time; }
-	int get_start_time() { return start_time; }
-	int get_end_time() { return end_time; }
-	int get_run_time() { return (start_time - end_time); }
-	int get_cycle_start_time() { return cycle_start_time; }
-    
-	std::vector<instruct_t> get_registers ();
-	resourceType get_resource_status() { return resource_held; }
-	status get_status() { return currentStatus;}
-	std::size_t get_program_counter()	{ return programCounter; }
-	
-	std::size_t get_page_table_length()		{ return pageTable.pages.size(); }
-	bool is_valid_page(std::size_t pageNumber)	{ return pageTable.pages[pageNumber].first; }	
-	std::size_t get_frame(std::size_t pageNumber)	{ return pageTable.pages[pageNumber].second; }
-	std::size_t pop_lru_page() { page_stack.pop_back(); return page_stack.back(); }
-	std::pair<bool, size_t> get_page_table_entry(std::size_t pageNumber);
+{
+	sectionSizes[section::INSTRUCTION] = instruct;
+	sectionSizes[section::INPUT] = inp;
+	sectionSizes[section::OUTPUT] = out;
+	sectionSizes[section::TEMP] = temp;
 
-	// SETTERS
-	void set_priority(int priorityIn);
-	void set_registers(std::vector<instruct_t> source);
-	//void acquire_resource(status code);
-	void set_status(status code);
-	void set_wait_time(int newtime);
-	void set_start_time(int startIn);
-	void set_end_time(int endIn);
-	void set_cycle_start_time(int cycleIn);
-    
-	void set_ram_address(std::size_t address);
-	void set_program_counter(std::size_t new_pc);
-	void increment_PC() { programCounter += 4; }
-	
-	void set_page_table_entry(std::size_t entry, bool valid, std::size_t frame);
-	void update_page_stack(std::size_t pageNumber);
+	//Give the page table enough pages to fit the process
+	pageTable = PCB::PageTable((get_end_address() / PAGE_SIZE + 1) * PAGE_SIZE);
+}
+		// GETTERS
+		int get_priority() { return priority; }
+		unsigned int get_pid() { return pid; }
+		std::size_t get_ram_address() { return ramAddress; }
+		std::size_t get_disk_address() { return diskAddress; }
+		std::size_t get_inp_address() { return sectionSizes[INSTRUCTION]; }
+		std::size_t get_out_address() { return sectionSizes[INSTRUCTION] + sectionSizes[INPUT]; }
+		std::size_t get_temp_address() { return sectionSizes[INSTRUCTION] + sectionSizes[INPUT] + sectionSizes[OUTPUT]; }
+		std::size_t get_end_address() { return sectionSizes[INSTRUCTION] + sectionSizes[INPUT] + sectionSizes[OUTPUT] + sectionSizes[TEMP]; }
+		int get_wait_time() { return wait_time; }
+		int get_start_time() { return start_time; }
+		int get_end_time() { return end_time; }
+		int get_run_time() { return (start_time - end_time); }
+		int get_cycle_start_time() { return cycle_start_time; }
+
+		std::vector<instruct_t> get_registers();
+		resourceType get_resource_status() { return resource_held; }
+		status get_status() { return currentStatus; }
+		std::size_t get_program_counter() { return programCounter; }
+
+		std::size_t get_page_table_length() { return pageTable.pages.size(); }
+		bool is_valid_page(std::size_t pageNumber) { return pageTable.pages[pageNumber].first; }
+		std::size_t get_frame(std::size_t pageNumber) { return pageTable.pages[pageNumber].second; }
+		std::size_t pop_lru_page() { std::size_t lru = page_stack.back(); page_stack.pop_back(); return lru; }
+		std::pair<bool, size_t> get_page_table_entry(std::size_t pageNumber);
+
+		// SETTERS
+		void set_priority(int priorityIn);
+		void set_registers(std::vector<instruct_t> source);
+		//void acquire_resource(status code);
+		void set_status(status code);
+		void set_wait_time(int newtime);
+		void set_start_time(int startIn);
+		void set_end_time(int endIn);
+		void set_cycle_start_time(int cycleIn);
+
+		void set_ram_address(std::size_t address);
+		void set_program_counter(std::size_t new_pc);
+		void increment_PC() { programCounter += 4; }
+
+		void set_page_table_entry(std::size_t entry, bool valid, std::size_t frame);
+		void update_page_stack(std::size_t pageNumber);
 
 private:
 	unsigned int pid;
